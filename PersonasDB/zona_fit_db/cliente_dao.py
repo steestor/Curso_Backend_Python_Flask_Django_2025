43ab76b1260fadb43f54523440e166cdaf0fd6ca
sesparza <sesparza@iti.es>
2025-05-02 21:30:49 +0200

    añadiendo insertar en  Sistema Zona Fit

--- PersonasDB/zona_fit_db/cliente_dao.py
+++ PersonasDB/zona_fit_db/cliente_dao.py
@@ -4,7 +4,7 @@ from cliente import Cliente
 
 class ClienteDAO:
     SELECCIONAR =  "select * from cliente order by id"
-    INSERTAR = "insert into cliente values (nombre, apellido, membresia) values (%s, %s, %s)"
+    INSERTAR = "insert into cliente(nombre, apellido, membresia) values (%s, %s, %s)"
     ACTUALIZAR = "update cliente set nombre=%s, apellido=%s, membresia=%s where id=%s"
     ELIMINAR = "delete from cliente where id=%s"
 
@@ -31,7 +31,32 @@ class ClienteDAO:
                 cursor.close()
                 Conexion.liberar_conexion(conexion)
 
+    @classmethod
+    def insertar(cls, cliente):
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+
+            valores = (cliente.nombre, cliente.apellido, cliente.membresia)
+            cursor.execute(ClienteDAO.INSERTAR, valores)
+            conexion.commit()
+
+            return cursor.rowcount
+
+        except Exception as e:
+            print(f"No se ha podido insertar el cliente {e}")
+        finally:
+            if conexion is not None:
+                cursor.close()
+                Conexion.liberar_conexion(conexion)
+
 if __name__ == "__main__":
+    # Insertar cliente
+    cliente1 = Cliente(nombre="Alejandra", apellido="Tellez", membresia=300)
+    clientes_actualizados = ClienteDAO.insertar(cliente1)
+    print(f"Clientes insertados: {clientes_actualizados}")
+
     # Seleccionar los clientes
     clientes = ClienteDAO.seleccionar()
     for cliente in clientes:
